ca333f120199e938ff295547b4397c83e4c40bfe
STCB <21@stcb.cc>
2025-09-19 16:59:33 +0200

    Proper package, Adapter CRASHES

--- nade/__init__.py
+++ nade/__init__.py
@@ -1,10 +1,12 @@
 # nade/__init__.py
 from .mode_a import NadeByteLink
 from .mode_b import NadeAudioPort
+from .nade_adapter import Adapter
 
 __all__ = [
     "NadeByteLink",
-    "NadeAudioPort"
+    "NadeAudioPort",
+    "Adapter",
 ]
 
 __version__ = "0.1.0"
