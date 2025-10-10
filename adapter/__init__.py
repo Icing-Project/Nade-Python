e66bc770d0540c9227d89e54d6d1be3023f16c48
STCB <21@stcb.cc>
2025-10-10 11:32:54 +0200

    Fix adapter init (remove old modes)

--- adapter/__init__.py
+++ adapter/__init__.py
@@ -1,11 +1,7 @@
-from nade.modes.mode_a import NadeByteLink
-from nade.modes.mode_b import NadeAudioPort
 from .drybox_adapter import Adapter
 
 __all__ = [
-    "NadeByteLink",
-    "NadeAudioPort",
     "Adapter",
 ]
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
